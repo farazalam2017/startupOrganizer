b89bbf14c28adfbfc1633ccb4710d72b7aa5e6db
farazalam2017 <farazalam2017@gmail.com>
2024-12-11 08:24:58 +0530

    Initial commit

---
+++ startupfinder/lib/action.ts
@@ -0,0 +1,53 @@
+"use server";
+
+import { auth } from "@/auth";
+import { parseServerActionResponse } from "@/lib/utils";
+import slugify from "slugify";
+import { writeClient } from "@/sanity/lib/write-client";
+
+export const createPitch = async (formValues) => {
+  const session = await auth();
+
+  if (!session) {
+    return parseServerActionResponse({
+      error: "Not signed in",
+      status: "ERROR",
+    });
+  }
+
+  const { title, description, category, link, pitch } = formValues;
+  const slug = slugify(title, { lower: true, strict: true });
+
+  try {
+    const startup = {
+      _type: "startup",
+      title,
+      description,
+      category,
+      image: link,
+      slug: {
+        _type: "slug",
+        current: slug,
+      },
+      author: {
+        _type: "reference",
+        _ref: session.id,
+      },
+      pitch,
+    };
+
+    const result = await writeClient.create(startup);
+
+    return parseServerActionResponse({
+      ...result,
+      error: "",
+      status: "SUCCESS",
+    });
+  } catch (error) {
+    console.error("Create Pitch Error:", error);
+    return parseServerActionResponse({
+      error: error.message || "An unexpected error occurred",
+      status: "ERROR",
+    });
+  }
+};
